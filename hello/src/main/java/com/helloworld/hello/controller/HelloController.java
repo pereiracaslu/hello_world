e2c0d726194c16fa0bdeebc6115904d5cb8b3d34
pereiracaslu <lucascosta2512@gmail.com>
2022-08-29 18:12:07 -0300

    Criação de camadas Spring

--- hello/src/main/java/com/helloworld/hello/controller/HelloController.java
+++ hello/src/main/java/com/helloworld/hello/controller/HelloController.java
@@ -6,10 +6,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/hello")
+
 public class HelloController {
 	
-	@GetMapping
+	@GetMapping ("/hello1") //endpoint
 	public String hello() {
 		return "Hello Generention!";
 	}
+	
+	@GetMapping ("/hello2")
+	public String bsm() {
+		return "\t\tMentalidades:\n-Orientação ao futuro\n-Responsabilidade pessoal\n-Mentalidade de crescimento\n-Persistência\n\n"
+				+"\t\tHabilidades:\n-Trabalho em equipe\n-Orientação ao detalhe\n-Pró-atividade\r\n-Comunicação";
+	}
+	
+	@GetMapping ("/hello3")
+	public String objetivos() {
+		return "-Fixar o conhecimento de Poo e DataBase.\n-Introdução a Spring";
+	}
+	
 }
